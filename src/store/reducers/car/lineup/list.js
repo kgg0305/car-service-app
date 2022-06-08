0f9f427a678883739eabab942ab080a710d3dca0
kgg0305 <jonesko0305@gmail.com>
2022-06-08 11:05:27 +0800

    fix

--- src/store/reducers/car/lineup/list.js
+++ src/store/reducers/car/lineup/list.js
@@ -13,6 +13,7 @@ const SET_SEARCH = prefix + "SET_SEARCH";
 export const init = () => async (dispatch) => {
   try {
     const dataSource = await lineupService.getList(0);
+    const dataLength = await lineupService.getCount();
     const brandOptionList = await brandService.getOptionList();
     const groupOptionList = await groupService.getOptionList();
     const modelOptionList = await modelService.getOptionList();
@@ -21,6 +22,7 @@ export const init = () => async (dispatch) => {
       type: INIT,
       payload: {
         dataSource: dataSource,
+        dataLength: dataLength,
         brandOptionList: brandOptionList,
         groupOptionList: groupOptionList,
         modelOptionList: modelOptionList,
